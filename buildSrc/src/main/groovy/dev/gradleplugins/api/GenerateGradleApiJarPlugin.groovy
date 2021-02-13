20c8c00ce3f5ad0a0a686ec0d4bab0fa7bee1983
Daniel Lacasse <daniel@nokee.dev>
2021-02-12 22:32:21 -0500

    Remove bintray publishing usage

--- buildSrc/src/main/groovy/dev/gradleplugins/api/GenerateGradleApiJarPlugin.groovy
+++ buildSrc/src/main/groovy/dev/gradleplugins/api/GenerateGradleApiJarPlugin.groovy
@@ -1,19 +1,15 @@
 package dev.gradleplugins.api
 
-import com.jfrog.bintray.gradle.tasks.BintrayUploadTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.attributes.*
 import org.gradle.api.attributes.java.TargetJvmVersion
 import org.gradle.api.component.SoftwareComponentFactory
 import org.gradle.api.credentials.AwsCredentials
-import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.publish.maven.internal.artifact.FileBasedMavenArtifact
 
 import javax.annotation.Nullable
 import javax.inject.Inject
-import java.text.SimpleDateFormat
 
 import static dev.gradleplugins.GradleRuntimeCompatibility.*
 
@@ -72,60 +68,8 @@ abstract class GenerateGradleApiJarPlugin implements Plugin<Project> {
 					}
 				}
 			}
-			apply plugin: 'com.jfrog.bintray'
-			// Temporary workaround for https://github.com/bintray/gradle-bintray-plugin/issues/229
-			PublishingExtension publishing = project.extensions.getByType(PublishingExtension)
-			project.tasks.withType(BintrayUploadTask).configureEach {
-				doFirst {
-					publishing.publications.withType(MavenPublication).each { publication ->
-						File moduleFile = project.buildDir.toPath()
-								.resolve("publications/${publication.name}/module.json").toFile()
-
-						if (moduleFile.exists()) {
-							publication.artifact(new FileBasedMavenArtifact(moduleFile) {
-								@Override
-								protected String getDefaultExtension() {
-									return "module"
-								}
-							})
-						}
-					}
-				}
-			}
-
-			bintray {
-				user = resolveProperty(project, "BINTRAY_USER", "dev.gradleplugins.bintray.user")
-				key = resolveProperty(project, "BINTRAY_KEY", "dev.gradleplugins.bintray.key")
-				publications = publishing.publications.collect { it.name }
-
-				publish = true
-				override = true
-
-				pkg {
-					repo = 'distributions'
-					name = 'dev.gradleplugins:gradle-api'
-					desc = project.description
-					userOrg = 'gradle-plugins'
-					websiteUrl = 'https://gradleplugins.dev'
-					issueTrackerUrl = 'https://github.com/gradle-plugins/toolbox/issues'
-					vcsUrl = 'https://github.com/gradle-plugins/toolbox.git'
-					labels = ['gradle', 'gradle-api', 'gradle-plugins']
-					licenses = ['Apache-2.0']
-					publicDownloadNumbers = false
-
-					version {
-						released = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ").format(new Date())
-						// TODO: Sign artifacts
-						gpg {
-							sign = false
-							passphrase = resolveProperty(project, "GPG_PASSPHRASE", "dev.gradleplugins.bintray.gpgPassphrase")
-						}
-					}
-				}
-			}
 
 			tasks.register('uploadGradleApiJars') {
-//				dependsOn('bintrayUpload')
 				dependsOn('publish')
 			}
 		}
